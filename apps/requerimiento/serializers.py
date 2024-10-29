a4dcbe12e2604047adb5bd1c247e82796da8ca95
kevin garcia <developer.kevin.95@gmail.com>
2024-10-28 21:15:21 -0600

    agregando relaciones para listar requerimientos

--- apps/requerimiento/serializers.py
+++ apps/requerimiento/serializers.py
@@ -1,13 +1,17 @@
 from rest_framework import serializers
 from .models import Requerimiento, EstadoRequerimiento
+from apps.user.serializers import CustomUserSerializer
+from apps.proyecto.serializers import ProyectoSerializer
 
 class EstadoRequerimientoSerializer(serializers.ModelSerializer):
     class Meta:
         model = EstadoRequerimiento
-        fields = ['estadoRequerimiento']
+        fields = ['idEstadoRequerimiento','estadoRequerimiento']
 
 class RequerimientoSerializer(serializers.ModelSerializer):
     estadoRequerimiento = EstadoRequerimientoSerializer(read_only=True, source='idEstadoRequerimiento')  # Serializador anidado para estado
+    usuarioEncargado = CustomUserSerializer(read_only=True, source='idUsuarioEncargado')
+    proyecto = ProyectoSerializer(read_only=True, source='idProyecto')
 
     class Meta:
         model = Requerimiento
@@ -18,9 +22,11 @@ class RequerimientoSerializer(serializers.ModelSerializer):
             'fechaEstimadoEntrega',
             'fechaInicioPropuesto',
             'idUsuarioEncargado',
+            'usuarioEncargado',
             'idUsuarioRegistro',
             'idEstadoRequerimiento',
             'estadoRequerimiento',
             'idProyecto',
+            'proyecto',
         ]
         read_only_fields = ['idRequerimiento', 'fechaRegistro']
