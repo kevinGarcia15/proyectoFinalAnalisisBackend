51507d76c5f0852b0cdfe39e60da7cb8037be11f
kevin garcia <developer.kevin.95@gmail.com>
2024-11-22 19:05:31 -0600

    agregando manual tecnico y de usaurio

--- apps/user/views.py
+++ apps/user/views.py
@@ -34,46 +34,6 @@ class UserApi(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-    def put(self, request, id):
-        """
-        Esta función actualiza los detalles de un usuario existente utilizando su ID.
-        Toma los datos proporcionados en la solicitud y los actualiza en la base de datos.
-
-        Returns:
-            Response: Un objeto de respuesta con los detalles del usuario actualizado y un estado HTTP 204.
-        """
-        try:
-            user = User.objects.get(id=id)
-        except User.DoesNotExist:
-            return Response({"message": "User not found"}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = UserSerializer(user, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-
-    def delete(self, request, id):
-        """
-        Esta función cambia el estado de un usuario a 'inactivo', lo que es equivalente a eliminarlo
-        de forma lógica. El usuario no se elimina físicamente de la base de datos.
-
-        Returns:
-            Response: Un objeto de respuesta con un mensaje de éxito y un estado HTTP 204 NO CONTENT.
-        """
-        try:
-            user = User.objects.get(id=id)
-        except User.DoesNotExist:
-            return Response({"message": "User not found"}, status=status.HTTP_404_NOT_FOUND)
-        
-        user_delete = {'is_active': False}
-        serializer = UserSerializer(user, data=user_delete, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"message": "User deleted successfully"}, status=status.HTTP_204_NO_CONTENT)    
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 class Login(APIView):
     """
